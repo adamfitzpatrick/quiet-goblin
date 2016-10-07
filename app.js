9179ca6d065d171addf2425da689172e782c5170
Adam Fitzpatrick <adam.fitzpatrick@hobsons.com>
2016-10-07 13:24:26 -0700

    Use separate qa tables for qa environment

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 "use strict";
 
+if (process.env.EB_INSTANCE_ENV_TAG) {
+    process.env.NODE_ENV = process.env.EB_INSTANCE_ENV_TAG;
+}
+
 let bluebird = require("bluebird");
 let request = bluebird.promisify(require("request"));
 let endpoint = require("./src/configuration/app-config").dynamo_endpoint;
 
 request(endpoint).then(() => {
     require("./src/server.js")();
-}).catch(() => {
-    console.error(`Database (${endpoint}) cannot be reached.`);
+}).catch((err) => {
+    console.error(err);
 });
--- deploy_vars.sh
+++ deploy_vars.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 export ELASTIC_BEANSTALK_LABEL=$(echo $TRAVIS_BRANCH | grep "^[0-9].[0-9].[0-9]-\?[a-zA-Z]*\.\?[0-9]*$")
-SEMVER=$(echo $ELASTIC_BEANSTALK_LABEL | cut -d - -f 1)
-if [ "$ELASTIC_BEANSTALK_LABEL" = "$SEMVER" ]; then
-  export ENV_TAG="prod"
-else
-  export ENV_TAG=$(echo $ELASTIC_BEANSTALK_LABEL | cut -d - -f 2 | cut -d . -f 1 | grep "dev")
-fi
+export SEMVER=$(echo $ELASTIC_BEANSTALK_LABEL | cut -d - -f 1)
+export ENV_TAG=$(if [ ${#ELASTIC_BEANSTALK_LABEL} -gt 0 -a $ELASTIC_BEANSTALK_LABEL = $SEMVER ]; then echo prod; else echo $ELASTIC_BEANSTALK_LABEL | cut -d - -f 2 | cut -d . -f 1 | grep "qa"; fi)
 export ELASTIC_BEANSTALK_ENV="quiet-goblin-"$ENV_TAG
